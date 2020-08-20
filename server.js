f70c06e14948294e956892d4dfbf4f2204e8239a
allielynne20 <allielynne20@gmail.com>
2020-08-20 13:34:55 -0400

    completed the GET all candidates, GET one candidate, and DELETE a candidate api call

--- server.js
+++ server.js
@@ -41,32 +41,50 @@ app.get('/api/candidates', (req, res) => {
 });
 
 // GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                    WHERE id =?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 // Delete a candidate
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err) {
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates 
+                    WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
 //     if (err) {
 //         console.log(err);
 //     }
-//     console.log(this.changes);
+//     console.log(result, this.lastID);
 // });
-
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
-});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
